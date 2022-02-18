5eb9ca597648fd1ab87e05a779b3b8a6af8f74e4
baolan2005 <baolan2005@gmail.com>
2022-02-18 14:49:34 +0100

    init project

---
+++ JcampViewer/JcampViewer/JcampViewer.h
@@ -0,0 +1,18 @@
+//
+//  JcampViewer.h
+//  JcampViewer
+//
+//  Created by Lan Le on 18.02.22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for JcampViewer.
+FOUNDATION_EXPORT double JcampViewerVersionNumber;
+
+//! Project version string for JcampViewer.
+FOUNDATION_EXPORT const unsigned char JcampViewerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JcampViewer/PublicHeader.h>
+
+
